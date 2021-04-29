0ad7305d7d242ab27c02d21a2d355f6916d2786f
Yan Kin Luong <yan.kin.luong@me.com>
2021-04-29 16:44:08 -0500

    flask

---
+++ app.py
@@ -0,0 +1,12 @@
+
+# import the Flask dependency
+from flask import Flask
+
+
+
+# create a new Flask instance called app:
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return 'Hello world'
